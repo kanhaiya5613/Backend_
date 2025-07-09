acec296c129deeced1efdda25559edf739419a89
kanhaiya kumar <kk7077574@gmail.com>
2025-07-09 12:08:43 +0530

    add npm

---
+++ 3.npm/index.js
@@ -0,0 +1,9 @@
+//var generatename = require("sillyname");
+
+import generatename  from "sillyname"; // if you use type module in package.json
+var sillynName =  generatename();
+console.log("my  Name is:", sillynName);
+
+// import superheroes from "superheroes";
+// var superherosName = superheroes();
+// console.log("I am ", superherosName)
